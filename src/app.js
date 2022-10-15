208307b3946715e112161e4ccd791e7336251375
david9833 <dagalisteo@gmail.com>
2022-10-15 11:29:48 +0000

    Mi primer commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,58 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//Arrays con elementos
+var iconos = ["♦", "♠", "♥", "♣"];
+var numero = ["AS", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//Variables que generan los elementos aleatorios
+var randomNumber = numero[Math.floor(Math.random() * numero.length)];
+var randomIcono = iconos[Math.floor(Math.random() * iconos.length)];
+
+//Selección de los elementos
+var icono = document.querySelectorAll(".icono");
+var numero = document.querySelector(".numero");
+
+//Función principal al cargar la página
+window.addEventListener("load", function() {
+  generaIcono();
+  generaNumero();
+});
+
+//Funciones asignadas al evento cargar principal
+function generaIcono() {
+  if (randomIcono == "♦") {
+    icono[0].innerHTML = "♦";
+    icono[0].classList.add("diamante");
+    icono[1].innerHTML = "♦";
+    icono[1].classList.add("diamante");
+    numero.classList.add("colorNumero");
+  } else if (randomIcono == "♠") {
+    icono[0].innerHTML = "♠";
+    icono[1].innerHTML = "♠";
+  } else if (randomIcono == "♥") {
+    icono[0].innerHTML = "♥";
+    icono[0].classList.add("corazon");
+    icono[1].innerHTML = "♥";
+    icono[1].classList.add("corazon");
+    numero.classList.add("colorNumero");
+  } else if (randomIcono == "♣") {
+    icono[0].innerHTML = "♣";
+    icono[1].innerHTML = "♣";
+  }
+}
+
+function generaNumero() {
+  if (randomNumber >= 2 || randomNumber <= 9) {
+    numero.innerHTML = randomNumber;
+  } else if (randomNumber == "J") {
+    numero.innerHTML = "J";
+  } else if (randomNumber == "Q") {
+    numero.innerHTML = "Q";
+  } else if (randomNumber == "K") {
+    numero.innerHTML = "K";
+  } else if (randomNumber === "AS") {
+    numero.innerHTML = randomIcono;
+    numero.classList.add("as");
+  }
+}
